607bfc00a60dc0ff90285530067d2a3cd3516ec2
hawjk777zzz <razyparastoree@gmail.com>
2026-09-07 19:33:45 -0300

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -32,8 +32,7 @@ app.use(
 app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  clerkMiddleware()
+app.use(clerkMiddleware());
 
 app.use("/api", router);
 
